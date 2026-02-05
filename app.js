780cd0ecaff2b1d8a9fd605460a103474a030350
Luca Striano <luca.striano90@gmail.com>
2026-02-05 16:17:57 +0100

    add assett statico e imgs

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // importo express nel file 
 
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
+//asset statici di expressjs (middleware)
+
+app.use(express.static('public'));
